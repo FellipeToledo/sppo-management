34cd30cedc018cceed3a436f048a983332f11ad4
FellipeToledo <fellipetoledo.dev@gmail.com>
2024-04-18 15:55:08 -0300

    initial commit

---
+++ server/src/main/java/com/azvtech/sppo/server/SppoServerApplication.java
@@ -0,0 +1,13 @@
+package com.azvtech.sppo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SppoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SppoServerApplication.class, args);
+	}
+
+}
